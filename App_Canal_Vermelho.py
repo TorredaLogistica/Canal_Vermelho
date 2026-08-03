5a0c569989cf1e9adf76087091398a0a810c2bd9
TorredaLogistica <logistica.armazenagem.claro@gmail.com>
2026-08-03 18:24:19 -0300

    Refactor EMAIL_GERENTE_TESTE assignments
    Consolidate multiple email assignments into a single line.

--- App_Canal_Vermelho.py
+++ App_Canal_Vermelho.py
@@ -517,10 +517,8 @@ if visualization == "📧 Enviar por e-mail":
             "Rio de Janeiro": "EDGAR.RIBEIRODASILVA@claro.com.br",
             "Campinas": "demis.lamego@claro.com.br",
         }
-        EMAIL_GERENTE_TESTE = "andre.dikman@claro.com.br"
-        EMAIL_GERENTE_TESTE = "demis.lamego@claro.com.br"
-        EMAIL_GERENTE_TESTE = "EDGAR.RIBEIRODASILVA@claro.com.br"
-
+        EMAIL_GERENTE_TESTE = "andre.dikman@claro.com.br";"demis.lamego@claro.com.br";"EDGAR.RIBEIRODASILVA@claro.com.br"
+      
         referencia_envio = period_label(selected_period)
         base_email = filtered_all[filtered_all["MesRef"] == selected_period].copy()
         cds_base = set(base_email["CD Origem"].dropna().astype(str))
